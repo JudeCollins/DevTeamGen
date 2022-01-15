155e79403621bcad542b73020fe9aa38159234fc
Jude Collins <jude.collins13@yahoo.com>
2022-01-15 13:39:30 -0500

    html gen

--- src/page-template.js
+++ src/page-template.js
@@ -53,4 +53,69 @@ const createIntern = intern => {
     </div>
   `
 }
+// page data
+generatePage = data => {
+    teamBoxesArr = [];
+  
+    for (let i = 0; i < data.length; i++) {
+      const employee = data[i];
+      const role = employee.getRole(); 
+      console.log(role);
+      
+      if (role === "Manager") {
+        const managerBox = createManager(employee);
+  
+        teamBoxesArr.push(managerBox);
+      }
+  
+      if (role === "Engineer") {
+        const engineerBox = createEngineer(employee);
+  
+        teamBoxesArr.push(engineerBox);
+      }
+  
+      if (role === "Intern") {
+        const internBox = createIntern(employee);
+  
+        teamBoxesArr.push(internBox);
+      }
+    }
+  
+    const teamBoxes = teamBoxesArr.join("");
+  
+    const generateTeam = createTeamHTML(teamBoxes);
+    return generateTeam;
+  }
+
+  // Team boxes
+const createTeamHTML = teamBoxes => {
+    return `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profiles</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+      </head>
+      <body>
+        <header>
+          <div class="text-center p-3 mb-2 bg-danger">
+            <h1>My Team</h1>
+          </div>
+        </header>
+        <section class="d-flex flex-row flex-wrap justify-content-center">
+          ${teamBoxes}
+        </section>
+        
+      </body>
+      </html>
+    `
+  }
+  
+  module.exports = generatePage;
+  
 
